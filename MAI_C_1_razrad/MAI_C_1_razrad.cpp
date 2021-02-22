de35c0253436c5a698723bcb88f07b297c936ae8
Олег Павлов <oleg101-101@mail.ru>
2021-02-22 13:48:28 +0300

    Добавьте файлы проекта.

---
+++ MAI_C_1_razrad/MAI_C_1_razrad.cpp
@@ -0,0 +1,30 @@
+﻿// MAI_C_1_razrad.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    uint32_t val,del=1;
+    cin >> val;
+    //cout << val;
+    uint8_t i=0;
+    while (del<val)
+    {
+        i++;
+        del = del * 10;
+        //cout << val << ' ' << del << endl;
+    }
+    del = del / 10;
+    //cout << del;
+    while (del > 0)
+    {
+        cout << val / del << endl;
+        val = val % del;
+        del = del / 10;
+        //cout << val << ' ' << del << endl;
+    }
+
+}
