a73eae0d9986bb17f5d6d7a26d54a814750d7e4c
Олег Павлов <oleg101-101@mail.ru>
2021-02-22 14:06:33 +0300

    text menu added

--- MAI_C_1_razrad/MAI_C_1_razrad.cpp
+++ MAI_C_1_razrad/MAI_C_1_razrad.cpp
@@ -1,16 +1,15 @@
-﻿// MAI_C_1_razrad.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
-
 
 int main()
 {
     uint32_t val,del=1;
+    uint8_t i = 0;
+
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число" << endl;
     cin >> val;
-    //cout << val;
-    uint8_t i=0;
+    
     while (del<val)
     {
         i++;
@@ -19,11 +18,18 @@ int main()
     }
     del = del / 10;
     //cout << del;
+    cout << val << '=';
     while (del > 0)
     {
-        cout << val / del << endl;
+        cout << val / del;
         val = val % del;
         del = del / 10;
+        i--;
+        cout << "*10^" << (int)i;
+        if (del > 0)
+        {
+            cout << '+';
+        }
         //cout << val << ' ' << del << endl;
     }
 
